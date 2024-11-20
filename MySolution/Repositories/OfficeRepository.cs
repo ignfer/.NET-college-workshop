af72133067135335f20845cac334d18c9bc854b8
IgnFer <nachofernandezuru@gmail.com>
2024-11-20 01:37:34 -0300

    Add: Use cases implementation
    - add client to queue
    - visualize waiting clients
    - visualize amount of waiting clients
    - visualize average waiting time
    - visualize stats

--- MySolution/Repositories/OfficeRepository.cs
+++ MySolution/Repositories/OfficeRepository.cs
@@ -7,16 +7,18 @@ namespace MySolution.Repositories
 {
 	public class OfficeRepository
 	{
-		private string _connectionString = "Data Source=sqlite-database.db";
+			private string _connectionString = "Data Source=sqlite-database.db";
 
-		public List<OfficePrev> GetOffices()
-		{
+			public List<OfficePrev> GetOffices()
+			{
+			
 			var offices = new List<OfficePrev>();
+			/*
 			var c = new SqliteConnection(_connectionString);
 			using (var connection = new SqliteConnection(_connectionString))
 			{
 				connection.Open();
-				string query = "SELECT id, Name FROM office";
+				string query = "SELECT id, Name FROM Offices";
 
 				using (var command = new SqliteCommand(query, connection))
 				using (var reader = command.ExecuteReader())
@@ -24,7 +26,7 @@ namespace MySolution.Repositories
 					while (reader.Read())
 					{
 						var office = new OfficePrev(
-							id: reader.GetInt32(0),            
+							id: reader.GetInt32(0),
 							name: reader.GetString(1),
 							inProgressDesks: new List<DeskPrev>(),
 							waitingClients: new List<ClientPrev>()
@@ -34,8 +36,8 @@ namespace MySolution.Repositories
 					}
 				}
 			}
-
+			*/
 			return offices;
+			}
 		}
 	}
-}
